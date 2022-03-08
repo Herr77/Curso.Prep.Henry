7c9ad131932aa7d6184fc5c6dbfde0e25de6af9b
Herr77 <hernanprieto7@gmail.com>
2022-03-08 16:48:08 -0300

    hice esto

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -19,7 +19,7 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
-}
+}hoaa
 
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
